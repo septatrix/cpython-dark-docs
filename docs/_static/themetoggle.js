fe1401429715502cefdb928260e95cf039778b0e
Septatrix <24257556+Septatrix@users.noreply.github.com>
2020-04-21 23:44:38 +0200

    Update

--- docs/_static/themetoggle.js
+++ docs/_static/themetoggle.js
@@ -18,18 +18,22 @@ function toggleTheme() {
 }
 
 function updateTheme() {
+  const buttons = Array.from(document.getElementsByClassName('theme-toggle'))
   switch (currentTheme) {
     case 'light':
       pydocthemeDark.media = 'not all'
       pygmentsDark.media = 'not all'
-      break
+      buttons.forEach(e => e.value = "Toggle theme (light)")
+      break;
     case 'dark':
       pydocthemeDark.media = 'all'
       pygmentsDark.media = 'all'
-      break
+      buttons.forEach(e => e.value = "Toggle theme (dark)")
+      break;
     default:
       // auto
       pydocthemeDark.media = '(prefers-color-scheme: dark)'
       pygmentsDark.media = '(prefers-color-scheme: dark)'
+      buttons.forEach(e => e.value = "Toggle theme (auto)")
   }
 }
